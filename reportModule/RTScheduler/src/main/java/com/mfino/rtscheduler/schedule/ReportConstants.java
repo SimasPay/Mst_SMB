2900a6123ba65628f6138ebbdc68a463dd299bbb
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-02-03 08:56:44 +0000

    Modified to generate on demand reports from Adminapplication. Adding the missed files in previous commit.

---
+++ reportModule/RTScheduler/src/main/java/com/mfino/rtscheduler/schedule/ReportConstants.java
@@ -0,0 +1,13 @@
+package com.mfino.rtscheduler.schedule;
+
+public class ReportConstants {
+	
+	public static String PARAMETER_QUERY_START_TIME = "StartTime";
+	public static String PARAMETER_QUERY_END_TIME = "EndTime";
+	public static String PARAMETER_DISPLAY_START_TIME = "DStartTime";
+	public static String PARAMETER_DISPLAY_END_TIME = "DEndTime";
+	public static String PARAMETER_FOOTER_MESSAGE = "FooterMessage";
+	public static String PARAMETER_REPORT_TYPE = "ReportType";
+	public static String PARAMETER_REPORT_GENERATED_BY = "ReportGeneratedBy";
+	public static String PARAMETER_REPORT_GENERATED_TIME = "ReportGeneratedTime";
+}
