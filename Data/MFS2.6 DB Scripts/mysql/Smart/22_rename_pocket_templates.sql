2cdeb546cd620a9e13896484c0bde3b366588501
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-02-04 04:59:36 +0000

    Modified For E&Y Audit requirements. 1. Need to stop cashout and transfer transactions for non-kyc subscriber. 2. need to restrict the KYC upgrade level to only unbanked. ........

---
+++ Data/MFS2.6 DB Scripts/mysql/Smart/22_rename_pocket_templates.sql
@@ -0,0 +1,4 @@
+update pocket_template set description='Registered Uangku user' where description='Emoney-UnBanked';
+update pocket_template set description='Non-registered Uangku user' where description='Uangku Emoney-Non-KYC';
+update pocket_template set description='Non-Uangku user' where description='Emoney-UnRegistered';
+
