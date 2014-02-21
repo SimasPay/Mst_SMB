58ff21587f1f4bcfa9914777350470e234ea4470
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-02-21 12:17:58 +0000

    new favorite categories added for DIMO “transfertoBankFavCatId"	>4< “prePaid_PLN_CatId"		>5< "prePaid_HandPhone_CatId">6< “postPaid_PLN_CatId"	 	>7< "postPaid_HandPhone_CatId">8<

---
+++ Data/MFS2.6 DB Scripts/mysql/Hub_Emoney/51_favorite_categories_for_DIMO.sql
@@ -0,0 +1,7 @@
+-- Script to add new favorite categories for DIMO
+
+INSERT INTO favorite_category (Id,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Version,CategoryName,DisplayName) VALUES (4,now(),'System',now(),'System',0,'TransferToBank','Transfer To Bank');
+INSERT INTO favorite_category (Id,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Version,CategoryName,DisplayName) VALUES (5,now(),'System',now(),'System',0,'Prepaid PLN','Prepaid PLN');
+INSERT INTO favorite_category (Id,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Version,CategoryName,DisplayName) VALUES (6,now(),'System',now(),'System',0,'PrePaid Phone','PrePaid Phone');
+INSERT INTO favorite_category (Id,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Version,CategoryName,DisplayName) VALUES (7,now(),'System',now(),'System',0,'Postpaid PLN','Postpaid PLN');
+INSERT INTO favorite_category (Id,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Version,CategoryName,DisplayName) VALUES (8,now(),'System',now(),'System',0,'Postpaid Phone','Postpaid Phone');
