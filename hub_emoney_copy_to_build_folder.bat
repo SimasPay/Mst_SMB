178ed98959656e9ec29f051d87c6f0202cf1b196
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-03-11 13:12:07 +0000

    Modified to include the billspayment in build path Modified the batch file for generating the hub binaries to a separate directory

--- hub_emoney_copy_to_build_folder.bat
+++ hub_emoney_copy_to_build_folder.bat
@@ -11,6 +11,7 @@ goto End
 
 mkdir %1\DBscripts\
 mkdir %1\tomcat\
+mkdir %1\tomcat\mfino_conf\
 mkdir %1\servicemix\
 mkdir %1\servicemix\mfino_conf\
 mkdir %1\servicemix\jpos
@@ -23,6 +24,8 @@ copy Web\Scheduler\target\Scheduler.war %1\tomcat\
 copy Web\webapi\target\webapi.war %1\tomcat\
 rem copy Web\ReportScheduler\target\ReportScheduler.war %1\tomcat\
 rem copy Web\TransactionMonitorTool\target\TransactionMonitorTool.war %1\tomcat\
+copy Core\settings\hub\mfino.properties %1\tomcat\mfino_conf\
+copy Core\settings\hub\languageTranslation.json %1\tomcat\mfino_conf\
 
 copy Commons\HierarchyService\target\HierarchyService-0.1-SNAPSHOT.jar %1\servicemix
 copy Commons\TransactionApi\target\TransactionApi-0.1-SNAPSHOT.jar %1\servicemix
@@ -34,6 +37,8 @@ copy MfinoCoreEngine\ISODefinitions\target\ISODefinitions-0.1-SNAPSHOT.jar %1\se
 copy MfinoCoreEngine\MCECore\target\MCECore-0.1-SNAPSHOT.jar %1\servicemix
 copy MfinoCoreEngine\NotificationService\target\NotificationService-0.1-SNAPSHOT.jar %1\servicemix
 copy MfinoCoreEngine\JPOSComponent\target\JPOSComponent-0.1-SNAPSHOT.jar %1\servicemix
+rem copy MfinoCoreEngine\ISOComponent\target\ISOComponent-0.1-SNAPSHOT.jar %1\servicemix
+copy MfinoCoreEngine\IntegrationInterfaces\target\IntegrationInterfaces-0.1-SNAPSHOT.jar %1\servicemix
 
 copy MfinoCoreEngine\MCEConfiguration\src\main\resources\META-INF\spring\mce_auto_reversal_configuration.xml %1\servicemix\
 copy MfinoCoreEngine\MCEConfiguration\src\main\resources\META-INF\spring\mce_backend_configuration.xml %1\servicemix\
@@ -46,11 +51,19 @@ copy MfinoCoreEngine\MCEConfiguration\src\main\resources\META-INF\spring\mce_sch
 copy BSIM\BSIMSMS\target\BSIMSMS-0.1-SNAPSHOT.jar %1\servicemix\
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_sms_configuration.xml %1\servicemix\
 
-copy Core\settings\smart\database_config.properties %1\servicemix\mfino_conf\
+copy Core\settings\hub\database_config.properties %1\servicemix\mfino_conf\
 copy MfinoCoreEngine\mfino_conf\bc2qmap.cfg %1\servicemix\mfino_conf\
 copy MfinoCoreEngine\mfino_conf\externalcodedescriptions.xml %1\servicemix\mfino_conf\
 copy MfinoCoreEngine\mfino_conf\mce.properties %1\servicemix\mfino_conf\
-copy Core\settings\smart\mfino.properties %1\servicemix\mfino_conf\
+copy Core\settings\hub\mfino.properties %1\servicemix\mfino_conf\
+
+rem BSM integration (bank)
+copy Smart\BSMISO8583\target\BSMISO8583-0.1-SNAPSHOT.jar %1\servicemix
+copy Smart\SmartConfiguration\src\main\resources\META-INF\spring\mce_bsm_iso_configuration.xml %1\servicemix\
+copy Smart\SmartConfiguration\src\main\jpos_cfg\iso87ascii-bsm.xml %1\servicemix\jpos\jpos_cfg\
+copy Smart\SmartConfiguration\src\main\jpos\connection_config_bsm.xml %1\servicemix\jpos\deploy\
+copy Smart\SmartConfiguration\src\main\jpos\jpos_config_bsm.xml %1\servicemix\jpos\deploy\
+copy Smart\SmartConfiguration\src\main\jpos\mux_config_bsm.xml %1\servicemix\jpos\deploy\
 
 rem NFC related Jars
 copy Smart\NFCISO8583\target\NFCISO8583-0.1-SNAPSHOT.jar %1\servicemix
@@ -65,5 +78,21 @@ copy GTBank\BillPayments\target\BillPayments-0.1-SNAPSHOT.jar %1\servicemix\
 copy Hub\HubConfiguration\src\main\resources\META-INF\spring\mce_billpay_configuration.xml %1\servicemix
 copy Hub\HubConfiguration\src\main\resources\META-INF\spring\mce_hub_xml_configuration.xml %1\servicemix
 copy Hub\HubXMLRPC\target\HubXMLRPC-0.1-SNAPSHOT.jar %1\servicemix
+
+rem Interbank transfer
+copy MfinoCoreEngine\InterBankService\target\InterBankService-0.1-SNAPSHOT.jar %1\servicemix\
+copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_interbanktransfer_configuration.xml %1\servicemix\
+copy BSIM\BSIMInterBankTransfer\target\BSIMInterBankTransfer-0.1-SNAPSHOT.jar %1\servicemix\
+
+rem Flashiz Related Files
+copy Hub\FlashizISO8583\target\FlashizISO8583-0.1-SNAPSHOT.jar %1\servicemix
+copy Hub\FlashizBillPayment\target\FlashizBillPayment-0.1-SNAPSHOT.jar %1\servicemix
+copy Hub\HubConfiguration\src\main\resources\META-INF\spring\mce_flashiz_iso_configuration.xml %1\servicemix\
+copy Hub\HubConfiguration\src\main\resources\META-INF\spring\mce_flashiz_payment_xml_configuration.xml %1\servicemix\
+copy Hub\HubConfiguration\src\main\jpos\connection_config_flashiz.xml %1\servicemix\jpos\deploy\
+copy Hub\HubConfiguration\src\main\jpos\jpos_config_flashiz.xml %1\servicemix\jpos\deploy\
+copy Hub\HubConfiguration\src\main\jpos\mux_config_flashiz.xml %1\servicemix\jpos\deploy\
+copy Hub\HubConfiguration\src\main\jpos_cfg\iso87ascii-flashiz.xml %1\servicemix\jpos\jpos_cfg\
+
 
 :End
