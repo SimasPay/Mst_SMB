4fd84c0a080ff4cdb6801c7b758a6645fd329ca9
amarnadh_n <amarnadh_n@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-05-15 11:01:14 +0000

    Bayarnet bill Payments unit test code to check http communication

---
+++ Hub/BayarBillPayments/src/test/java/com/mfino/bayarnet/BayernetHttpClientTest.java
@@ -0,0 +1,66 @@
+package com.mfino.bayarnet;
+
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.mfino.bayar.service.BayarHttpConnector;
+import com.mfino.bayar.service.BayarWebServiceResponse;
+import com.mfino.bayar.service.SecondaryBayarHttpConnector;
+
+
+/**
+ * 
+ * @author Amar
+ *
+ */
+public class BayernetHttpClientTest {
+
+	public static void main(String[] args)
+	{
+		String url = "https://202.51.252.110/demo/h2h/";
+		String method = "inquiry_bill";
+				
+		BayarHttpConnector mfinoHttpConnector = new BayarHttpConnector();
+		mfinoHttpConnector.setTimeout("30000");
+		mfinoHttpConnector.setUrl(url);
+		
+		BayarWebServiceResponse response = null;
+		try {
+			
+			List <NameValuePair> parameters = new ArrayList <NameValuePair>();
+			parameters.add(new BasicNameValuePair("partner_id", "smartfren"));
+			parameters.add(new BasicNameValuePair("api_key", "1D02640903DF5E9126EDB9FC46C6FB70"));
+			parameters.add(new BasicNameValuePair("product_code", "TAG031"));
+			parameters.add(new BasicNameValuePair("bill_number", "6576587"));
+			parameters.add(new BasicNameValuePair("reference_id", ""));
+			mfinoHttpConnector.setParams(new HashMap<String,String>());
+			response = mfinoHttpConnector.sendHttpsRequest(method, parameters);
+			
+//			parameters = new ArrayList <NameValuePair>();
+//			parameters.add(new BasicNameValuePair("partner_id", "smartfren"));
+//			parameters.add(new BasicNameValuePair("api_key", "1D02640903DF5E9126EDB9FC46C6FB70"));
+//			parameters.add(new BasicNameValuePair("payment_code", response.getPaymentCode()));
+//			mfinoHttpConnector.setParams(new HashMap<String,String>());
+//			response = mfinoHttpConnector.sendHttpRequest("pay_bill", parameters);
+			
+			
+		} catch (SocketTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		System.out.println("Response:"+ response);
+	}
+}
