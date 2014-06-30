f4e8e2984f09c225b8ee90d689e69e8b369deb63
amarnadh_n <amarnadh_n@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-06-30 09:59:02 +0000

    refs #401 Corrected a typo

--- Data/MFS2.6 DB Scripts/mysql/212_DailyFlashizReport.sql
+++ Data/MFS2.6 DB Scripts/mysql/212_DailyFlashizReport.sql
@@ -1,3 +1,3 @@
 
- INSERT INTO offline_report (Version,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Name,ReportSql,ReportClass,TriggerEnable,IsDaily,IsMonthly, Isonline) VALUES (1,now(),'system',now(),'system','FLASHiZReport',NULL,'FLASHiZReport',0,1,0,0);
+ INSERT INTO offline_report (Version,LastUpdateTime,UpdatedBy,CreateTime,CreatedBy,Name,ReportSql,ReportClass,TriggerEnable,IsDaily,IsMonthly, IsOnlineReport) VALUES (1,now(),'system',now(),'system','FLASHiZReport',NULL,'FLASHiZReport',0,1,0,0);
  
