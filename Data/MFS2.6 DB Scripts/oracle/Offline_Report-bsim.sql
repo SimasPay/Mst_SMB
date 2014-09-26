a321963c25a5da69063829df9c2ab9491097b3b9
venkat_b <venkat_b@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-09-26 10:52:56 +0000

    updated scripts ...

--- Data/MFS2.6 DB Scripts/oracle/Offline_Report-bsim.sql
+++ Data/MFS2.6 DB Scripts/oracle/Offline_Report-bsim.sql
@@ -1,10 +1,9 @@
-Delete from `offline_report`;
+Delete from OFFLINE_REPORT;
 
-INSERT INTO `offline_report` (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('1', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Registration Report', 'RegistrationReport', '0', '1', '1', '1');
+Insert into OFFLINE_REPORT (ID,VERSION,LASTUPDATETIME,UPDATEDBY,CREATETIME,CREATEDBY,NAME,REPORTCLASS,TRIGGERENABLE,ISONLINEREPORT,ISDAILY,ISMONTHLY) values (38,1,to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system',to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system','Registration Report','RegistrationReport',1,1,0,0);
 
-INSERT INTO `offline_report` (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('2', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Transaction Monthly Report', 'TransactionMonthlyReport', '0', '1', '1', '1');
+Insert into OFFLINE_REPORT (ID,VERSION,LASTUPDATETIME,UPDATEDBY,CREATETIME,CREATEDBY,NAME,REPORTCLASS,TRIGGERENABLE,ISONLINEREPORT,ISDAILY,ISMONTHLY) values (39,1,to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system',to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system','Transaction Monthly Report','TransactionMonthlyReport',1,1,0,0);
 
-INSERT INTO offline_report` (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('3', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Activation Report', 'ActivationReport', '0', '1', '1', '1');
+Insert into OFFLINE_REPORT (ID,VERSION,LASTUPDATETIME,UPDATEDBY,CREATETIME,CREATEDBY,NAME,REPORTCLASS,TRIGGERENABLE,ISONLINEREPORT,ISDAILY,ISMONTHLY) values (40,1,to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system',to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system','Activation Report','ActivationReport',1,1,0,0);
 
-INSERT INTO offline_report` (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('4', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'TransactionReport', 'Transaction Report', '0', '1', '1', '1');
-
+Insert into OFFLINE_REPORT (ID,VERSION,LASTUPDATETIME,UPDATEDBY,CREATETIME,CREATEDBY,NAME,REPORTCLASS,TRIGGERENABLE,ISONLINEREPORT,ISDAILY,ISMONTHLY) values (41,1,to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system',to_timestamp('08-MAR-13 12.37.49.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'system','Transaction Report','TransactionReport',1,1,0,0);
