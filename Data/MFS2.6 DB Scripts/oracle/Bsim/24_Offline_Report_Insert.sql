dd0bf2367e44e9921b715660855d16285a67b338
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-10-09 12:44:33 +0000

    Modified to script to remove the "`".

--- Data/MFS2.6 DB Scripts/oracle/Bsim/24_Offline_Report_Insert.sql
+++ Data/MFS2.6 DB Scripts/oracle/Bsim/24_Offline_Report_Insert.sql
@@ -1,10 +1,12 @@
 Delete from offline_report;
 
-INSERT INTO offline_report (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('1', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Registration Report', 'RegistrationReport', '0', '1', '0', '1');
+INSERT INTO offline_report (ID, Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('1', '1', sysdate, 'system', sysdate, 'system', 'Registration Report', 'RegistrationReport', '0', '1', '0', '1');
 
-INSERT INTO offline_report (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('2', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Transaction Monthly Report', 'TransactionMonthlyReport', '0', '0', '1', '1');
+INSERT INTO offline_report (ID, Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('2', '1', sysdate, 'system', sysdate, 'system', 'Transaction Monthly Report', 'TransactionMonthlyReport', '0', '0', '1', '0');
 
-INSERT INTO offline_report (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('3', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Activation Report', 'ActivationReport', '0', '1', '0', '1');
+INSERT INTO offline_report (ID, Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('3', '1', sysdate, 'system', sysdate, 'system', 'Activation Report', 'ActivationReport', '0', '1', '0', '1');
 
-INSERT INTO offline_report (`ID`, `Version`, `LastUpdateTime`, `UpdatedBy`, `CreateTime`, `CreatedBy`, `Name`, `ReportClass`, `TriggerEnable`, `IsDaily`, `IsMonthly`, `IsOnlineReport`) VALUES ('4', '1', '2014-09-19 10:47:26', 'system', '2014-09-19 10:47:26', 'system', 'Transaction Report', 'TransactionReport', '0', '1', '0', '1');
+INSERT INTO offline_report (ID, Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('4', '1', sysdate, 'system', sysdate, 'system', 'Transaction Report', 'TransactionReport', '0', '1', '0', '1');
+
+commit;
 
