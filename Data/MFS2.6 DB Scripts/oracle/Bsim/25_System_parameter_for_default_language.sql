0435ed97d38c6484099ace63f8ed1f3823ffd2fc
sreenath_s <sreenath_s@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-10-15 07:13:56 +0000

    Correcting an oracle script error .

--- Data/MFS2.6 DB Scripts/oracle/Bsim/25_System_parameter_for_default_language.sql
+++ Data/MFS2.6 DB Scripts/oracle/Bsim/25_System_parameter_for_default_language.sql
@@ -1,4 +1,6 @@
 
-DELETE FROM `system_parameters` WHERE ParameterName='default.language.of.subscriber';
+DELETE FROM system_parameters WHERE ParameterName='default.language.of.subscriber';
 
-INSERT INTO `system_parameters` (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, ParameterName, ParameterValue, Description) VALUES (1,sysdate,'System',sysdate,'system','default.language.of.subscriber','1','Default Language of Subscriber. 0-English, 1-Bahasa');
+INSERT INTO system_parameters (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, ParameterName, ParameterValue, Description) VALUES (1,sysdate,'System',sysdate,'system','default.language.of.subscriber','1','Default Language of Subscriber. 0-English, 1-Bahasa');
+
+commit;
