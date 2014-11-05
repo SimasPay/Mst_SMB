3d1e3d7d0cd080ab2cd91750fbbdaeb1645a8f96
hemanth_k <hemanth_k@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-11-05 11:55:02 +0000

    copy to build folder batch file change

--- bsim_copy_to_build_folder.bat
+++ bsim_copy_to_build_folder.bat
@@ -71,12 +71,14 @@ copy GTBank\BillPayments\target\BillPayments-0.1-SNAPSHOT.jar %1\servicemix
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_billpay_configuration.xml %1\servicemix\
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_billpay_configuration.xml %1\servicemix\
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_iso_configuration.xml %1\servicemix\
-copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_sms_configuration.xml %1\servicemix\
+rem copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_sms_configuration.xml %1\servicemix\
+copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_new_bsim_sms_configuration.xml %1\servicemix\
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_interbanktransfer_configuration.xml %1\servicemix\
 copy BSIM\BSIMBillPayment\target\BSIMBillPayment-0.1-SNAPSHOT.jar %1\servicemix\
 copy BSIM\BSIMInterBankTransfer\target\BSIMInterBankTransfer-0.1-SNAPSHOT.jar %1\servicemix\
 copy BSIM\BSIMISO8583\target\BSIMISO8583-0.1-SNAPSHOT.jar %1\servicemix\
-copy BSIM\BSIMSMS\target\BSIMSMS-0.1-SNAPSHOT.jar %1\servicemix\
+rem copy BSIM\BSIMSMS\target\BSIMSMS-0.1-SNAPSHOT.jar %1\servicemix\
+copy BSIM\NewBSIMSMS\target\NewBSIMSMS-0.1-SNAPSHOT.jar %1\servicemix\
 
 copy BSIM\FlashizISO8583\target\BSIMFlashizISO8583-0.1-SNAPSHOT.jar %1\servicemix\
 copy BSIM\BSIMConfiguration\src\main\resources\META-INF\spring\mce_bsim_flashiz_iso_configuration.xml %1\servicemix\
