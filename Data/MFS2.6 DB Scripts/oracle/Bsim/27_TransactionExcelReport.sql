a925c5f12a6f3190e9152fb71423eaffa05c2ea8
b_sunku <b_sunku@b74383d8-7005-449c-88d9-1e400cf8bab0>
2014-11-19 16:06:25 +0000

    Modified the offline report script

--- Data/MFS2.6 DB Scripts/oracle/Bsim/27_TransactionExcelReport.sql
+++ Data/MFS2.6 DB Scripts/oracle/Bsim/27_TransactionExcelReport.sql
@@ -1,3 +1,3 @@
-INSERT INTO offline_report (ID, Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('42', '1', sysdate, 'system', sysdate, 'system', 'Transaction Excel Report', 'TransactionExcelReport', '0', '1', '0', '1');
+INSERT INTO offline_report (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Name, ReportClass, TriggerEnable, IsDaily, IsMonthly, IsOnlineReport) VALUES ('1', sysdate, 'system', sysdate, 'system', 'Transaction Excel Report', 'TransactionExcelReport', '0', '0', '0', '1');
 
 commit;
