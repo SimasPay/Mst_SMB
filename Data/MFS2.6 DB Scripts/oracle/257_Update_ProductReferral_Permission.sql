958a33be28efcbdcb5a5827f1176802e4cc7086b
Gopalakrishna Thota <gopalakrishna.thota@rand-ind.com>
2016-03-23 13:10:29 +0530

    Provided Update Script for Showing ProductReferal link in Menu items

---
+++ Data/MFS2.6 DB Scripts/oracle/257_Update_ProductReferral_Permission.sql
@@ -0,0 +1,3 @@
+update PERMISSION_ITEM set permissiongroupid=1,description='ProductReferrals' where permission=23101
+update PERMISSION_ITEM set permissiongroupid=1,description='ProductReferrals View' where permission=23102
+commit;
