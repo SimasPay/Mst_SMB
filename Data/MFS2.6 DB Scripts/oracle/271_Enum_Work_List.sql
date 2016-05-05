917c80ebf707a01b3923637584cd9f1440397d2c
Gopalakrishna Thota <gopalakrishna.thota@rand-ind.com>
2016-05-05 13:01:02 +0530

    changed Tagname in Script file

--- Data/MFS2.6 DB Scripts/oracle/271_Enum_Work_List.sql
+++ Data/MFS2.6 DB Scripts/oracle/271_Enum_Work_List.sql
@@ -1,10 +1,10 @@
 Delete From enum_text where TagID =8457; 
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','1','Kartu Kredit','Kartu Kredit');
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','2','Kredit Kepemilikan Mobil','Kredit Kepemilikan Mobil');
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','3','Pinjaman Modal Kerja','Pinjaman Modal Kerja');
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','4','Kredit Investasi','Kredit Investasi');
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','5','Kredit Usaha Mikro','Kredit Usaha Mikro');
-INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','AdjustmentStatus','8457','6','Lainnya','Lainnya');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','1','Kartu Kredit','Kartu Kredit');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','2','Kredit Kepemilikan Mobil','Kredit Kepemilikan Mobil');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','3','Pinjaman Modal Kerja','Pinjaman Modal Kerja');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','4','Kredit Investasi','Kredit Investasi');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','5','Kredit Usaha Mikro','Kredit Usaha Mikro');
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','WorkList','8457','6','Lainnya','Lainnya');
 
 ALTER TABLE subscriber_addi_info  ADD OtherWork VARCHAR2(255);
 
