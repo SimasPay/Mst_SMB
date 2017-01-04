e89c1dae84ba9d02be5c2310125f5ceb5be79b76
bsunku <bala.sunku@rand-ind.com>
2017-01-04 14:44:20 +0530

    Modified for SMP-516:  Reversal of Transfer e-Money to e-Money - Unregistered Subsriber Case - MFS

--- Data/MFS2.6 DB Scripts/oracle/296_SMP_516_Reverse_charge_for_unregistered_transfer.sql
+++ Data/MFS2.6 DB Scripts/oracle/296_SMP_516_Reverse_charge_for_unregistered_transfer.sql
@@ -1,3 +1,6 @@
 update system_parameters set parametervalue = 'false' where parametername = 'reverse.charge.for.expired.transfer.to.unregistered';
 
+Delete from system_parameters where ParameterName = 'transfer.to.unregistered.expiry.time';
+INSERT INTO system_parameters (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, ParameterName, ParameterValue, Description) VALUES (1,sysdate,'System',sysdate,'system','transfer.to.unregistered.expiry.time','2','Transfer to UnRegistered Expiry Time (days)');
+
 commit;
