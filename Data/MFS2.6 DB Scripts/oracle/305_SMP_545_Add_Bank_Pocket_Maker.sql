93c5d9e627f173af7215c6d456fbbac3bf51f862
aruna <aruna.chikkam@rand-ind.com>
2017-01-23 18:34:05 +0530

    Modified Scriptf for SMP_545

--- Data/MFS2.6 DB Scripts/oracle/305_SMP_545_Add_Bank_Pocket_Maker.sql
+++ Data/MFS2.6 DB Scripts/oracle/305_SMP_545_Add_Bank_Pocket_Maker.sql
@@ -1,12 +1,14 @@
---Add permisson item 
-DELETE FROM PERMISSION_ITEM WHERE PERMISSION = 10250;
+--Add permisson item
 
-Insert into PERMISSION_ITEM (VERSION, LASTUPDATETIME, UPDATEDBY, CREATETIME, CREATEDBY, PERMISSION, ITEMTYPE, ITEMID, FIELDID, ACTION, PERMISSIONGROUPID, DESCRIPTION) values ('1', sysdate, 'system', sysdate, 'system', '10250', '1', 'emoneysub.add.bankpocket', 'default', 'default', '1', 'Add Bankpocket to Emoney Subscriber');
+Insert into PERMISSION_ITEM (VERSION, LASTUPDATETIME, UPDATEDBY, CREATETIME, CREATEDBY, PERMISSION, ITEMTYPE, ITEMID, FIELDID, ACTION, PERMISSIONGROUPID, DESCRIPTION) values ('1', sysdate, 'system', sysdate, 'system', '10250', '1', 'emoneysub.add.bankpocket', 'default', 'default', (select id from PERMISSION_GROUP where PERMISSIONGROUPNAME = 'Subscriber'), 'Add Bankpocket to Emoney Subscriber');
 
-INSERT INTO role_permission (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Role, Permission) VALUES('1', sysdate, 'system', sysdate, 'system', '1','10250');
+INSERT INTO role_permission (Version, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, Role, Permission) VALUES('1', sysdate, 'system', sysdate, 'system', (select id from Role where ENUMVALUE = 'Master_Admin'),'10250');
 
 ALTER TABLE SUBSCRIBER_UPGRADE_DATA ADD APPLICATIONID VARCHAR2(255);
 ALTER TABLE SUBSCRIBER_UPGRADE_DATA ADD BANKACCOUNTNUMBER VARCHAR2(255);
 ALTER TABLE SUBSCRIBER_UPGRADE_DATA  MODIFY FULLNAME  NULL;
+
+ALTER TABLE SUBSCRIBER_UPGRADE_DATA MODIFY SUBSCRIBER_ACTIVITY NOT NULL;
+ALTER TABLE SUBSCRIBER_UPGRADE_DATA MODIFY SUBSCRIBER_ACTIVITY_STATUS NOT NULL;
 
 COMMIT;
