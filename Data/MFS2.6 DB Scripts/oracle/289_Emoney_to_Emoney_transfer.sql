bc6bf8f649e64e318b51c72b6a30af4fe9eace50
thesigid <dimo@192.168.1.110>
2017-01-23 20:48:23 +0700

    SMP-456 [Simaspay e-Money] Transfer: e-Money to e-Money - MFSSMP-568 MFA Mode showing None for E2E transaction

--- Data/MFS2.6 DB Scripts/oracle/289_Emoney_to_Emoney_transfer.sql
+++ Data/MFS2.6 DB Scripts/oracle/289_Emoney_to_Emoney_transfer.sql
@@ -1,3 +1,8 @@
 INSERT INTO TRANSACTION_TYPE VALUES (TRANSACTION_TYPE_ID_SEQ.NEXTVAL,1,SYSDATE,'System',SYSDATE,'System',1,'E2ETransfer', 'E2E Transfer');
 INSERT INTO SERVICE_TRANSACTION (VERSION, LASTUPDATETIME, UPDATEDBY, CREATETIME, CREATEDBY, MSPID, SERVICEID, TRANSACTIONTYPEID) VALUES (1,SYSDATE,'System',SYSDATE,'System',1, (select id from service where SERVICENAME = 'Wallet'), (SELECT ID FROM TRANSACTION_TYPE WHERE TRANSACTIONNAME = 'E2ETransfer'));
+
+INSERT INTO mfa_transactions_info VALUES (mfa_transactions_info_ID_SEQ.nextval,1,sysdate,'System',sysdate,'System',1,(select id from service where SERVICENAME = 'Wallet')
+,(select id from transaction_type where TRANSACTIONNAME = 'E2ETransfer'),(select id from channel_code where CHANNELNAME = 'WebAPI'),1);
+
+
 commit;
