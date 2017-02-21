10a7a2e015da27c74ae34bc1c3eb7536bc316b9c
aruna <aruna.chikkam@rand-ind.com>
2017-02-21 17:14:07 +0530

    SMp-653 Internal Txn Type its showing empty when transferring to Uangku

---
+++ Data/MFS2.6 DB Scripts/oracle/314_SMP-653_EnumText_for_TransferToUangku.sql
@@ -0,0 +1,3 @@
+INSERT INTO enum_text (VERSION, LastUpdateTime, UpdatedBy, CreateTime, CreatedBy, LANGUAGE, TagName, TagID, EnumCode, EnumValue, DisplayText) VALUES ('1',sysdate,'system',sysdate,'system','0','TransactionUICategory','5636','64','Transfer_To_Uangku','Transfer To Uangku');
+
+commit;
