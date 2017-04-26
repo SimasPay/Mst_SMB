afcaf51e740ff0385aae1d8d706c00ef2ba26b64
thesigid <thesigid@gmail.com>
2017-04-26 15:21:20 +0700

    Delete 321_SMP-787_NewTransactionType_for_ReversalTransfertoUnregistered.sql
    No Need this script mfa_transaction_info, because for this new transactiontype is automatic and does not depend on the otp part

--- Data/MFS2.6 DB Scripts/oracle/321_SMP-787_NewTransactionType_for_ReversalTransfertoUnregistered.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO TRANSACTION_TYPE VALUES (TRANSACTION_TYPE_ID_SEQ.NEXTVAL,1,SYSDATE,'System',SYSDATE,'System',1,'AutoReverseTransferToNonRegistered','Auto Reverse Transfer To Unregistered');
-
-INSERT INTO SERVICE_TRANSACTION(VERSION,LASTUPDATETIME,UPDATEDBY,CREATETIME,CREATEDBY,MSPID,SERVICEID,TRANSACTIONTYPEID)  VALUES (1,SYSDATE,'System',SYSDATE,'System',1, (SELECT ID FROM SERVICE WHERE SERVICENAME = 'Wallet'), (SELECT ID FROM TRANSACTION_TYPE WHERE TRANSACTIONNAME = 'AutoReverseTransferToNonRegistered'));
-
-INSERT INTO mfa_transactions_info VALUES (mfa_transactions_info_ID_SEQ.nextval,1,sysdate,'System',sysdate,'System',1,(select id from service where SERVICENAME = 'Wallet')
-,(select id from transaction_type where TRANSACTIONNAME = 'AutoReverseTransferToNonRegistered'),(select id from channel_code where CHANNELNAME = 'WebAPI'),1);
-
-commit;
