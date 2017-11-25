6ae35e9fa61b7a9c55dfc6114e3cc52602f3129a
Didik Hari K <didikhari38@gmail.com>
2017-11-25 23:05:27 +0700

    update notification bank transfer

---
+++ Data/MFS2.6 DB Scripts/oracle/232_update_notification_bank_transfer_failure.sql
@@ -0,0 +1,6 @@
+update NOTIFICATION set TEXT = 'Mohon maaf fitur/layanan ini sedang tidak dapat digunakan. Silahkan gunakan SimobiPlus/Internet Banking/ATM Bank Sinarmas.' where CODE = '89'
+commit;
+
+
+
+
